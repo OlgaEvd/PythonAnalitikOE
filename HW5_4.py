00f99ff295592bd19efd309b4d3543e690ca6caa
«Olga» <elfica@yandex.ru>
2022-10-04 06:04:00 +0800

    Реализация RLE алгоритм.

---
+++ HW5_4.py
@@ -0,0 +1,37 @@
+# ЗАДАЧА 4 Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+# Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+with open('textHW5_4_code.txt', 'w') as file:
+    file.write(input('Напишите текст необходимый для сжатия: '))
+with open('textHW5_4_code.txt', 'r') as file:
+    my_text = file.readline()
+    text_compression = my_text.split()
+
+print(my_text)
+
+
+def rle_encode(text):
+    enconding = ''
+    prev_char = ''
+    count = 1
+    if not text:
+        return ''
+
+    for char in text:
+        if char != prev_char:
+            if prev_char:
+                enconding += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            count += 1
+    else:
+        enconding += str(count) + prev_char
+        return enconding
+
+
+text_compression = rle_encode(my_text)
+
+with open('textHW5_4_decode.txt.txt', 'w') as file:
+    file.write(f'{text_compression}')
+print(text_compression)
